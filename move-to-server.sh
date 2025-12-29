f830d4f3f798e46565dac550827a97c49583a379
Mike Therien <mike@mikeondotnet.com>
2025-12-29 11:14:24 -0500

    Added move-to-server helper

---
+++ move-to-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <user@server> <remote-path>"
+    echo "Example: $0 k6shm@homebot.therien.family /home/k6shm"
+    exit 1
+fi
+
+SERVER="$1"
+REMOTE_PATH="$2"
+
+echo "Building image..."
+docker build -t famick/homemanagement:latest .
+
+echo "Saving image..."
+docker save famick/homemanagement:latest | gzip > homemanagement.tar.gz
+
+echo "Transferring to server..."
+scp homemanagement.tar.gz "$SERVER:$REMOTE_PATH"
+
+echo "Cleaning up local file..."
+rm homemanagement.tar.gz
+
+echo "Loading image on server..."
+ssh "$SERVER" "cd $REMOTE_PATH && gunzip -f homemanagement.tar.gz && docker load -i homemanagement.tar && rm homemanagement.tar"
+
+echo "Done!"
